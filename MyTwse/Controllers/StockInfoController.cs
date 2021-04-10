999ed56f75e3e97ab0c703b28fe8b36ec36327fd
yuhaoliao <h770320@gmail.com>
2021-04-10 16:22:07 +0800

    補上註解

--- MyTwse/Controllers/StockInfoController.cs
+++ MyTwse/Controllers/StockInfoController.cs
@@ -21,21 +21,36 @@ namespace MyTwse.Controllers
         {
             return _StockInfoService.GetStockInfos();
         }
+        /// <summary>
+        /// 依照證券代號 搜尋最近n天的資料
+        /// </summary>
+        /// <param name="stockCode"></param>
+        /// <param name="days"></param>
+        /// <returns></returns>
         [Route("StockInfo/Recent")]
         public List<StockInfo> GetStockInfoByRecent(string stockCode, int days = 5)
         {
             return _StockInfoService.GetStockInfoByRecent(stockCode, days);
         }
-
+        /// <summary>
+        /// 指定特定日期 顯示當天本益比前n名
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
         [Route("StockInfo/PE/Rank")]
         public List<StockInfo> GetStockPERank(DateTime date,int count = 100)
         {
             return _StockInfoService.GetStockPERank(date, count);
         }
+        /// <summary>
+        /// 新增股票資料
+        /// </summary>
+        /// <param name="model"></param>
         [HttpPost]
         [Route("StockInfo")]
         public void CreateStockInfoData(CreateStockViewModel model)
-        {
+        { 
             _StockInfoService.CreateStockInfoData(model.StartDate.Value, model.EndDate.Value);
         }
     }
